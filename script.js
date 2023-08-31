3b65818e4b8913f3ce6ce79034e70fc22376b52c
Neelasri <139204824+Neelasri@users.noreply.github.com>
2023-09-01 00:11:00 +0530

    task3

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript for interactive elements
+const playButton = document.querySelector('.play-button');
+
+
+playButton.addEventListener('click', () => {
+    alert('in the orginal netflix you have to provide credentials in the sign in');
+});
